49a1459c0617d87ee1c5fa9b2848e69d73926bd4
ThomasBreuer <sam@math.rwth-aachen.de>
2022-02-23 09:05:31 +0100

    adjusted the initialization
    Although this script is not really needed,
    as we had seen yesterday,
    this is a working version.
    
    (The idea to use Julia was apparently not good:
    Currently there are problems with its Tar.jl package
    for unpacking some of the archives.
    Also the functionality for uncompressing different formats
    looks not user friendly,
    and support for .zip archives seems to be missing.)

--- init_metadata.jl
+++ init_metadata.jl
@@ -1,8 +1,10 @@
 # Create the initial contents of `PackageDistro`.
-using SHA
-using JSON
+using CodecZlib
 using DataStructures
 using HTTP
+using JSON
+using SHA
+using Tar
 
 info = read(expanduser("~/package-infos.json"), String)
 info = JSON.parse(info; dicttype = SortedDict)
@@ -17,13 +19,44 @@ println(pkgname)
     downl = String(req.body)
     localpath = basename(arch)
     write(localpath, downl)
-    sha = open(localpath) do f
+    archivesha = open(localpath) do f
             sha256(f)
           end
-    push!(info[pkgname], "Sha256" => bytes2hex(sha))
+
+    tar_gz = open(localpath)
+    tar = GzipDecompressorStream(tar_gz)
+    # dir = Tar.extract(tar, "tmpdir") # does not work on some archives ...
+    # list = Tar.list(tar, strict = false) # does not work on some archives ...
+    # dirnam = list[1].path
+    io = IOBuffer()
+    if endswith(localpath, ".zip")
+#T ugly hack
+      run(pipeline(`unzip -ql $localpath`, stdout = io))
+      str = String(take!(io))
+      spl = split( str, "\n")
+      dirnam = spl[3]
+      dirnam = dirnam[(findlast(' ', dirnam)+1):(findfirst('/', dirnam)-1)]
+      cmd = `unzip $localpath $dirnam/PackageInfo.g`
+    else
+      run(pipeline(`tar tf $localpath`, stdout = io))
+      str = String(take!(io))
+      spl = split( str, "\n")
+      dirnam = spl[1]
+      dirnam = dirnam[1:(findfirst('/', dirnam)-1)]
+      cmd = `tar xf $localpath $dirnam/PackageInfo.g` # do not use `xvzf`!
+    end
+    run(cmd)
+    close(tar)
+    pkginfopath = joinpath(dirnam, "PackageInfo.g")
+    packageinfosha = open(pkginfopath) do f
+            sha256(f)
+          end
+    push!(info[pkgname], "ArchiveSha256" => bytes2hex(archivesha))
+    push!(info[pkgname], "PackageInfoSha256" => bytes2hex(packageinfosha))
+    rm(dirnam, recursive = true)
     isdir(pkgname) || mkdir(pkgname)
     formattedstr = json(info[pkgname], 2)
-    write("$pkgname/$pkgname.json", formattedstr)
+    write("$pkgname/meta.json", formattedstr)
     rm(localpath)
   else
     println("file $arch not found")
